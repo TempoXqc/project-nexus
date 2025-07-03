f952f43343e3118a9489baa6ca82d915d3bdedaf
TempoXqc <peterdescamps.quebec@gmail.com>
2025-07-03 11:32:46 -0400

    first commit

--- apps/app.tsx
+++ apps/app.tsx
@@ -1,5 +1,5 @@
 // Uncomment this line to use CSS modules
-// import styles from './app.module.css';
+// import styles from './apps.module.css';
 import NxWelcome from './nx-welcome';
 
 import { Route, Routes, Link } from 'react-router-dom';
